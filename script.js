a6f81577138bff99abd69428b4cee7f84c4e5ad6
Malerator <112635636+Malerator@users.noreply.github.com>
2022-10-11 17:51:27 +0300

    fixed

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 "use strict"
 
-import {data} from "./emoji.js";
+// import {data} from "./emoji.js";
+
+
+const urlEmoji = "https://emoji.ymatuhin.workers.dev/"
+const data = await getData(urlEmoji)
+const grid = document.querySelector(".grid")
+const inp = document.querySelector("input")
+const form = document.forms[0]
+
+
+async function getData(urlEmoji) {
+let response = await fetch(urlEmoji);
+let urlData = await response.json();
+return urlData
+}
 
 function repeats(arr) {
   data.forEach((elem) => {
     elem.keywords = [...new Set(elem.keywords.split(" "))].join(" ");
   });
 }
-
-
-const grid = document.querySelector(".grid")
 
 function createCard(cardObj) {
   const card = document.createElement("div")
@@ -33,19 +44,49 @@ function createCard(cardObj) {
 
   return card;
 }
-
 repeats(data)
-
 data.forEach(elem => grid.append(createCard(elem)));
 
-const inp = document.querySelector("input")
-const form = document.forms[0]
-form.addEventListener("submit", (event) => event.preventDefault()) ///предотвращает отправку
+form.addEventListener("submit", (event) => event.preventDefault()) ///предотвращает отправку формы
 inp.addEventListener("change", inpHand)
+
 function inpHand(event) {
-let res = event.target.value
-let result = data.filter((elem) => elem.keywords.includes(res.toLowerCase().trim().replace(/\s/g,'')) || elem.title.includes(res.toLowerCase().trim()))
-grid.innerHTML = "";
-result.forEach((elem) => grid.append(createCard(elem)));
+let res = event.target.value.toLowerCase().trim();
+res = res.split(" ");
+let search = res.map((elem) => data.filter((el) => el.keywords.includes(elem) || el.title.includes(elem)));
+
+grid.innerHTML = '';
+
+[...new Set(search.flat(2))].forEach((elem) => grid.append(createCard(elem)));
+}
+
+function checkPosition(grid) {
+  // Нам потребуется знать высоту документа и высоту экрана:
+  const height = document.grid.offsetHeight
+  const screenHeight = window.innerHeight
+
+  // Они могут отличаться: если на странице много контента,
+  // высота документа будет больше высоты экрана (отсюда и скролл).
+
+  // Записываем, сколько пикселей пользователь уже проскроллил:
+  const scrolled = window.scrollY
+
+  // Обозначим порог, по приближении к которому
+  // будем вызывать какое-то действие.
+  // В нашем случае — четверть экрана до конца страницы:
+  const threshold = height - screenHeight / 4
+
+  // Отслеживаем, где находится низ экрана относительно страницы:
+  const position = scrolled + screenHeight
+
+  if (position >= threshold) {
+    // Если мы пересекли полосу-порог, вызываем нужное действие.
+data.forEach(elem => grid.append(createCard(elem)));
+;(() => {
+  window.addEventListener('scroll', checkPosition)
+  window.addEventListener('resize', checkPosition)
+})()
+
+  }
 }
 
